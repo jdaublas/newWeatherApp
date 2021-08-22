54e09e6840e7b43dddde619a0cfcbec79c4fb777
jdaublas <87877758+jdaublas@users.noreply.github.com>
2021-08-22 17:20:48 -0600

    Added temperature selectors

--- src/app.js
+++ src/app.js
@@ -24,9 +24,11 @@ function formatDate(timestamp) {
 }
 
 //Display temperature
+
 function displayTemperature(response) {
+  celciusTemperature = response.data.main.temp;
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
   let weatherDescription = document.querySelector("#description");
@@ -45,19 +47,42 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+//searching url
 function search(city) {
   let apiKey = "6dfa8826ffc57f6f6137d36396323455";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
+//searching cityinput
 function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
 }
 
-search("New York");
+function showFaherenheitTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let faherenheitTemperature = (celciusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(faherenheitTemperature);
+}
+
+function showCelciusTemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celciusTemperature);
+}
+
+let celciusTemperature = null;
 
 let form = document.querySelector("#search-form");
-addEventListener("submit", handleSubmit);
+form.addEventListener("submit", handleSubmit);
+
+let faherenheitLink = document.querySelector("#faherenheit-link");
+faherenheitLink.addEventListener("click", showFaherenheitTemp);
+
+let celciusLink = document.querySelector("#celcius-link");
+celciusLink.addEventListener("click", showCelciusTemp);
+
+search("El Salvador");
